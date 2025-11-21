894c3d8c72d81517e9c3af96473b2c33032c0fcc
Shafira MD <shafiramd@Shafiras-MacBook-Air.local>
2025-11-21 04:12:24 -0500

    Add Rescue and Control subclass

---
+++ app/src/main/java/com/example/smartair/Rescue.java
@@ -0,0 +1,53 @@
+package com.example.smartair;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+
+public class Rescue extends PEF {
+    private String prePostStatus;
+    private double rating;
+
+    public Rescue() {}
+
+    public Rescue(LocalDateTime date, String id, double count, String prePostStatus, double rating) throws IllegalArgumentException {
+        super(date, id, count);
+        this.prePostStatus = prePostStatus;
+        if (rating < 0 || rating >10) {
+            throw new IllegalArgumentException("Rating must be between 0 and 10");
+        }
+        this.rating = rating;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Rescue(String date, String id, double count, String prePostStatus, double rating) throws DateTimeParseException, IllegalArgumentException {
+        super(date, id, count);
+        this.prePostStatus = prePostStatus;
+        if (rating < 0 || rating >10) {
+            throw new IllegalArgumentException("Rating must be between 0 and 10");
+        }
+        this.rating = rating;
+    }
+
+    public String getPrePostStatus() {return prePostStatus;}
+    public double getRating() {return rating;}
+
+    public void setPrePostStatus(String prePostStatus) {
+        if (!prePostStatus.equals("Better") && !prePostStatus.equals("Same") && !prePostStatus.equals("Worse")) {
+            throw new IllegalArgumentException("prePostStatus must be better, same, or worse");
+        }
+        this.prePostStatus = prePostStatus;
+    }
+
+    public void setRating(double rating) {
+        if (rating<0 || rating>10) {
+            throw new IllegalArgumentException("Rating must be between 0 and 10");
+        }
+        this.rating = rating;
+    }
+
+}
