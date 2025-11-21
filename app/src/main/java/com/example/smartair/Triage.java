2b739e706b2bbdbfed290b0b20042b802a5afff6
nathankevin <nathanael.kevin@ifortepay.id>
2025-11-21 01:00:29 -0500

    create triage and symptom subclass

---
+++ app/src/main/java/com/example/smartair/Triage.java
@@ -0,0 +1,43 @@
+package com.example.smartair;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+public class Triage extends Item {
+    private boolean emergency;
+    private LocalDateTime endDate;
+    private List<String> symptompList;
+    private List<String> pefList;
+    private List<String> rescueList;
+
+    public Triage() {}
+
+    public Triage(String id, LocalDateTime startDate, boolean emergency, LocalDateTime endDate,
+                  List<String> symptompList, List<String> pefList, List<String> rescueList) {
+        super(startDate, id);
+        try {
+            this.endDate = LocalDateTime.parse(endDate);
+        } catch (DateTimeParseException e) {
+            System.out.println("Error parsing date: " + e.getMessage());
+            this.endDate = null;
+        }
+        this.emergency = emergency;
+        this.symptompList = symptompList;
+        this.pefList = pefList;
+        this.rescueList = rescueList;
+    }
+
+    public boolean getEmergency() { return emergency;}
+    public LocalDateTime getEndDate() { return endDate;}
+    public List<String> getSymptompList() { return symptompList;}
+    public List<String> getPefList() { return pefList;}
+    public List<String> getRescueList() { return rescueList;}
+
+    public void setEmergency(boolean emergency) { this.emergency = emergency;}
+    public void setEndDate(LocalDateTime endDate) { this.endDate = endDate;}
+    public void setSymptompList(List<String> symptompList) { this.symptompList = symptompList;}
+    public void setPefList(List<String> pefList) { this.pefList = pefList;}
+    public void setRescueList(List<String> rescueList) { this.rescueList = rescueList;}
+
+}
