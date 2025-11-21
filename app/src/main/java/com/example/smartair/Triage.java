ae6263270baab887a09e6db840e7dca61b12cbf5
nathankevin <nathanael.kevin@ifortepay.id>
2025-11-21 01:25:44 -0500

    fixed type

--- app/src/main/java/com/example/smartair/Triage.java
+++ app/src/main/java/com/example/smartair/Triage.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class Triage extends Item {
     private boolean emergency;
     private LocalDateTime endDate;
-    private List<String> symptompList;
+    private List<String> symptomList;
     private List<String> pefList;
     private List<String> rescueList;
 
     public Triage() {}
 
     public Triage(String id, LocalDateTime startDate, boolean emergency, String endDate,
-                  List<String> symptompList, List<String> pefList, List<String> rescueList) {
+                  List<String> symptomList, List<String> pefList, List<String> rescueList) {
         super(startDate, id);
         try {
             this.endDate = LocalDateTime.parse(endDate);
@@ -23,20 +23,20 @@ public class Triage extends Item {
             this.endDate = null;
         }
         this.emergency = emergency;
-        this.symptompList = symptompList;
+        this.symptomList = symptomList;
         this.pefList = pefList;
         this.rescueList = rescueList;
     }
 
     public boolean getEmergency() { return emergency;}
     public LocalDateTime getEndDate() { return endDate;}
-    public List<String> getSymptompList() { return symptompList;}
+    public List<String> getSymptompList() { return symptomList;}
     public List<String> getPefList() { return pefList;}
     public List<String> getRescueList() { return rescueList;}
 
     public void setEmergency(boolean emergency) { this.emergency = emergency;}
     public void setEndDate(LocalDateTime endDate) { this.endDate = endDate;}
-    public void setSymptompList(List<String> symptompList) { this.symptompList = symptompList;}
+    public void setSymptomList(List<String> symptomList) { this.symptomList = symptomList;}
     public void setPefList(List<String> pefList) { this.pefList = pefList;}
     public void setRescueList(List<String> rescueList) { this.rescueList = rescueList;}
 
