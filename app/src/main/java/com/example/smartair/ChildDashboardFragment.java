90e220d1940c6887b2831fc74d8dc6deb654b099
Aaron <aaronbsunarto@gmail.com>
2025-11-24 07:50:12 -0500

    feat: created child dashboard fragment and xml classes

---
+++ app/src/main/java/com/example/smartair/ChildDashboardFragment.java
@@ -0,0 +1,78 @@
+package com.example.smartair;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class ChildDashboardFragment extends Fragment {
+    private TextView childNameStr, childDataStr;
+    private DatabaseReference databaseReference;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_child_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initializeViews(view);
+        databaseReference = FirebaseDatabase.getInstance("https://smartair-abd1d-default-rtdb.firebaseio.com/").getReference("medicineLogs");
+//        dateTime = LocalDateTime.now();
+//        setupClickListeners();
+        setupRecyclerView();
+        fetchDataFromFirebase();
+    }
+
+    private void initializeViews(View view) {
+        childNameStr = view.findViewById(R.id.childName);
+        childDataStr = view.findViewById(R.id.childData);
+    }
+
+    private void setupRecyclerView() {
+    }
+
+    private void fetchDataFromFirebase() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+}
