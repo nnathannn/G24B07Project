03b5a59b0796981b22daf6283f33f2909ac3b9aa
Aaron <aaronbsunarto@gmail.com>
2025-11-24 10:32:46 -0500

    removed methods from technique class

--- app/src/main/java/com/example/smartair/Technique.java
+++ app/src/main/java/com/example/smartair/Technique.java
@@ -5,7 +5,6 @@ import com.google.firebase.database.PropertyName;
 public class Technique extends Item {
     @PropertyName("high-quality")
     private boolean highQuality;
-    private boolean[] feedbacks = new boolean[5];
 
     public Technique() {
     }
@@ -19,25 +18,4 @@ public class Technique extends Item {
     public boolean getHighQuality() { return highQuality; }
     @PropertyName("high-quality")
     public void setHighQuality(boolean highQuality) { this.highQuality = highQuality;}
-
-    public void newSession() {
-        for (int i = 0; i < feedbacks.length; i++) {
-            feedbacks[i] = false;
-        }
-    }
-
-    public void setFeedbackAtIndex(int index, boolean value) {
-        feedbacks[index] = value;
-    }
-
-    public void checkSessionQuality() {
-        boolean value = true;
-        for (boolean feedback : feedbacks) {
-            if (!feedback) {
-                value = false;
-                break;
-            }
-        }
-        setHighQuality(value);
-    }
 }
