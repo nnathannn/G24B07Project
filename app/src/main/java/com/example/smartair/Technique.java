eee371a2ee160570362cd12197aa25f51b6a7edb
Aaron <aaronbsunarto@gmail.com>
2025-11-25 11:37:03 -0500

    fix: resolved conflicts

--- app/src/main/java/com/example/smartair/Technique.java
+++ app/src/main/java/com/example/smartair/Technique.java
@@ -1,8 +1,10 @@
 package com.example.smartair;
 
+import com.google.firebase.database.PropertyName;
+
 public class Technique extends Item {
+    @PropertyName("high-quality")
     private boolean highQuality;
-    private boolean[] feedbacks = new boolean[5];
 
     public Technique() {
     }
@@ -12,27 +14,9 @@ public class Technique extends Item {
         this.highQuality = highQuality;
     }
 
+    @PropertyName("high-quality")
     public boolean getHighQuality() { return highQuality; }
+    @PropertyName("high-quality")
     public void setHighQuality(boolean highQuality) { this.highQuality = highQuality;}
 
-    public void newSession() {
-        for (int i = 0; i < feedbacks.length; i++) {
-            feedbacks[i] = false;
-        }
-    }
-
-    public void setFeedbackAtIndex(int index, boolean value) {
-        feedbacks[index] = value;
-    }
-
-    public void checkSessionQuality() {
-        boolean value = true;
-        for (boolean feedback : feedbacks) {
-            if (!feedback) {
-                value = false;
-                break;
-            }
-        }
-        highQuality = value;
-    }
 }
