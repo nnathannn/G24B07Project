867cbc6280312b32f3aecaac99b7d89cb041889a
nathankevin <nathanael.kevin@ifortepay.id>
2025-11-27 00:38:05 -0500

    feat/history-page

--- app/src/main/java/com/example/smartair/ParentHistoryFragment.java
+++ app/src/main/java/com/example/smartair/ParentHistoryFragment.java
@@ -2,63 +2,86 @@ package com.example.smartair;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ParentHistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ParentHistoryFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<String> filters;
+    private ArrayAdapter<String> filterAdapter;
+    private Spinner spinner;
+    private RecyclerView recycler;
+    private List<Item> itemList;
+    private ItemAdapter itemAdapter;
 
     public ParentHistoryFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ParentHistoryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ParentHistoryFragment newInstance(String param1, String param2) {
-        ParentHistoryFragment fragment = new ParentHistoryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        filters = Arrays.asList("Symptoms", "Triggers", "Zones", "Triages", "Medicines");
+        filterAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, filters);
+        filterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        itemList = new ArrayList<>();
+
         return inflater.inflate(R.layout.fragment_parent_history, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        spinner = view.findViewById(R.id.spinner);
+        spinner.setAdapter(filterAdapter);
+        recycler = view.findViewById(R.id.HistoryRecycler);
+        recycler.setHasFixedSize(True);
+        recycler.setAdapter(createAdapter("Symptoms", "2000-01-01", LocalDate.now().toString()));
+
+        Button applyFilter = view.findViewById(R.id.HistoryFilterSubmit);
+        Button resetFilter = view.findViewById(R.id.HistoryFilterReset);
+
+        applyFilter.setOnClickListener(v -> {
+            String filter = spinner.getSelectedItem().toString();
+            String start = view.findViewById(R.id.HistoryStartDate).getText().toString();
+            String end = view.findViewById(R.id.HistoryEndDate).getText().toString();
+
+        }
+    }
+
+    private ItemAdapter createAdapter(String type, String start, String end) {
+        itemList.clear();
+        switch (type) {
+            case "Symptoms":
+                itemAdapter = new HistorySymptomAdapter(itemList);
+            case "Zones":
+                itemAdapter = new HistoryZoneAdapter(itemList);
+            case "Triages":
+                itemAdapter = new HistoryTriageAdapter(itemList);
+            case "Medicines":
+                itemAdapter = new HistoryMedicineAdapter(itemList);
+        }
+    }
+
+    private List<String> getChildrenNames() {
+        List<String> children = new ArrayList<>();
+
     }
 }
