400fabc58c3103df499da8397d37297f75ec4375
Lucia Giovani <lucia@Lucias-MacBook-Air.local>
2025-11-30 19:23:47 -0500

    feat: create profile page

--- app/src/main/java/com/example/smartair/ProfileFragment.java
+++ app/src/main/java/com/example/smartair/ProfileFragment.java
@@ -1,14 +1,16 @@
 package com.example.smartair;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,8 +19,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
-    private FirebaseDatabase db = FirebaseDatabase.getInstance("https://smartair-abd1d-default-rtdb.firebaseio.com/");
+    private FirebaseDatabase db;
     private String userID;
+    private String type;
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private String curName;
+    private String curEmail;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -31,35 +39,158 @@ public class ProfileFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
-    @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // update later : retrieve userID from the user
+        db = FirebaseDatabase.getInstance("https://smartair-abd1d-default-rtdb.firebaseio.com/");
+
+        // update later
         userID = "11";
 
-        // check user type and load the right fragment
-        String [] userType = {"child", "parent", "provider"};
-        DatabaseReference ref = db.getReference();
+        // check user type
+        String [] userType = {"child-users", "parent-users", "provider-users"};
         for (String user : userType) {
-            ref.child(user).child(userID).equalTo(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            db.getReference().child(user).child(user).equalTo(userID)
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                     if (snapshot.exists()) {
-                        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-                        if (user.equals("child")) transaction.replace(R.id.fragmentContainerView, new ProfileChildFragment());
-                        if (user.equals("parent")) transaction.replace(R.id.parent_frame_layout, new ProfileParentFragment());
-                        if (user.equals("provider")) transaction.replace(R.id.providerHomeLayout, new ProfileProviderFragment());
-                        transaction.addToBackStack(null);
-                        transaction.commit();
+                        if (user.equals("child")) type = "child";
+                        if (user.equals("parent")) type = "parent";
+                        if (user.equals("provider")) type = "provider";
+                    }
+                    else {
+                        Toast.makeText(getContext(), "Error: User not found", Toast.LENGTH_SHORT).show();
                     }
                 }
 
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
-                    // error handling
+                    Toast.makeText(getContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
         }
+
+        name = view.findViewById(R.id.name);
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+
+        // set edit text to non-editable
+        disableEdit(name);
+        disableEdit(email);
+        // !!! password !!!
+
+        // show data
+        showData();
+
+        // edit data
+        enableEdit(name);
+        enableEdit(email);
+        // !!! password !!!
+
+        // save data
+        name.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                name.setFocusable(false);
+                name.setCursorVisible(false);
+                String nameValue = name.getText().toString();
+
+                if (!nameValue.equals(curName)) {
+                    new AlertDialog.Builder(getContext())
+                            .setTitle("Confirm Change")
+                            .setMessage("Are you sure you want to change your name?")
+                            .setPositiveButton("Yes", (dialog, which) -> {
+                                saveData("name", nameValue);
+                                curName = nameValue;
+                                Toast.makeText(getContext(), "Name changed to " + nameValue, Toast.LENGTH_SHORT).show();
+                            })
+                            .setNegativeButton("No", (dialog, which) -> {
+                                name.setText(curName);
+                                dialog.dismiss();
+                            })
+                            .show();
+                }
+            }
+        });
+
+        email.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                email.setFocusable(false);
+                email.setCursorVisible(false);
+                String emailValue = email.getText().toString();
+
+                if (!emailValue.equals(curEmail)) {
+                    new AlertDialog.Builder(getContext())
+                            .setTitle("Confirm Change")
+                            .setMessage("Are you sure you want to change your email?")
+                            .setPositiveButton("Yes", (dialog, which) -> {
+                                saveData("email", emailValue);
+                                curEmail = emailValue;
+                                Toast.makeText(getContext(), "Name changed to " + emailValue, Toast.LENGTH_SHORT).show();
+                            })
+                            .setNegativeButton("No", (dialog, which) -> {
+                                email.setText(curEmail);
+                                dialog.dismiss();
+                            })
+                            .show();
+                }
+            }
+        });
+
+        // show manage children when the parent open the profile
+        //
+    }
+
+    private void showData() {
+        DatabaseReference userRef = db.getReference().child(type + "-users").child(userID);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    curName = snapshot.child("name").getValue(String.class);
+                    curEmail = snapshot.child("email").getValue(String.class);
+                    name.setText(curName);
+                    email.setText(curEmail);
+                }
+                else {
+                    Toast.makeText(getContext(), "Error: User not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // !!! show password !!!
+    }
+
+    private void disableEdit(EditText editText) {
+        editText.setFocusable(false);
+        editText.setClickable(true);   // allows clicking to enable editing
+        editText.setCursorVisible(false);
+        editText.setKeyListener(null); // disable keyboard until clicked
+    }
+
+    private void enableEdit(EditText editText) {
+        editText.setOnClickListener(v -> {
+            editText.setFocusableInTouchMode(true);
+            editText.setFocusable(true);
+            editText.setCursorVisible(true);
+            editText.setKeyListener(new EditText(getContext()).getKeyListener()); // display keyboard
+        });
+    }
+
+    private void saveData(String type, String data) {
+        DatabaseReference typeRef = db.getReference().child(type + "-users").child(userID);
+        typeRef.child(type).setValue(data).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(getContext(), "Data saved", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getContext(), "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
