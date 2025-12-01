9d538e53e0aa4d09a3b9c621c3a4c6fcffa9f481
Lucia Giovani <lucia@lucias-air.home>
2025-12-01 13:59:03 -0500

    refactor: update triage class

--- app/src/main/java/com/example/smartair/Triage.java
+++ app/src/main/java/com/example/smartair/Triage.java
@@ -5,41 +5,34 @@ import java.time.format.DateTimeParseException;
 import java.util.List;
 
 public class Triage extends Item {
-    private String emergency;
+    private boolean emergency;
     private String endDate;
     private List<String> symptomList;
     private List<String> pefList;
-    private int rescue;
+    private List<String> rescueList;
 
     public Triage() {}
 
-    public Triage(String id, String startDate, String emergency, String endDate,
-                  List<String> symptomList, List<String> pefList, int rescue) throws IllegalArgumentException {
+    public Triage(String id, String startDate, boolean emergency, String endDate,
+                  List<String> symptomList, List<String> pefList, List<String> rescueList) {
         super(startDate, id);
         this.endDate = endDate;
         this.emergency = emergency;
         this.symptomList = symptomList;
         this.pefList = pefList;
-        if (rescue < 0) {
-            throw new IllegalArgumentException("Rescue must be a non-negative integer.");
-        }
-        this.rescue = rescue;
+        this.rescueList = rescueList;
     }
 
-    public String getEmergency() { return emergency;}
+    public boolean getEmergency() { return emergency;}
     public String getEndDate() { return endDate;}
     public List<String> getSymptomList() { return symptomList;}
     public List<String> getPefList() { return pefList;}
-    public int getRescueList() { return rescue;}
+    public List<String> getRescueList() { return rescueList;}
 
-    public void setEmergency(String emergency) { this.emergency = emergency;}
+    public void setEmergency(boolean emergency) { this.emergency = emergency;}
     public void setEndDate(String endDate) { this.endDate = endDate;}
     public void setSymptomList(List<String> symptomList) { this.symptomList = symptomList;}
     public void setPefList(List<String> pefList) { this.pefList = pefList;}
-    public void setRescueList(int rescue) throws IllegalArgumentException {
-        if (rescue < 0) {
-            throw new IllegalArgumentException("Rescue must be a non-negative integer.");
-        }
-        this.rescue = rescue;
-    }
+    public void setRescueList(List<String> rescueList) { this.rescueList = rescueList;}
+
 }
