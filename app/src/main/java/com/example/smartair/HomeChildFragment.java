75f3aa841892ec39ca8c6420f9d9eaae30411701
nathankevin <nathanael.kevin@ifortepay.id>
2025-12-02 03:59:41 -0500

    feat: created badge functionality

--- app/src/main/java/com/example/smartair/HomeChildFragment.java
+++ app/src/main/java/com/example/smartair/HomeChildFragment.java
@@ -11,33 +11,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeChildFragment extends Fragment {
 
-    private RecyclerView recyclerView;
-    private ItemAdapter badgeAdapter;
-    private List<Item> itemList;
+    private LinearLayout badgeLayout;
+    private String uid;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        uid = ((UIDProvider) requireActivity()).getUid();
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
+    }
 
-        //Copy the code up until fetchData() and change with respective view holders for recycler view
-        recyclerView = view.findViewById(R.id.badgeRecycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, true));
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        itemList = new ArrayList<>();
-        badgeAdapter = new BadgeAdapter(itemList);
-        recyclerView.setAdapter(badgeAdapter);
-
-        ItemAdapter.fetchData(badgeAdapter, "badge");
+        badgeLayout = view.findViewById(R.id.badgeHolder);
+        fetchBadge();
 
         Button buttonTriage = view.findViewById(R.id.buttonTriage);
         Button buttonMedicine = view.findViewById(R.id.buttonMedicine);
@@ -46,49 +49,12 @@ public class HomeChildFragment extends Fragment {
         Button buttonPEF = view.findViewById(R.id.buttonPEF);
         Button buttonProfile = view.findViewById(R.id.buttonProfile);
 
-        buttonTriage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new TriageFragment());
-            }
-        });
-
-        buttonMedicine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new MedicineFragment());
-            }
-        });
-
-        buttonDaily.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new SymptomFragment());
-            }
-        });
-
-        buttonTechnique.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new TechniqueFragment());
-            }
-        });
-
-        buttonPEF.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new PEFFragment());
-            }
-        });
-
-        buttonProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new ProfileFragment());
-            }
-        });
-
-        return view;
+        buttonTriage.setOnClickListener(v -> { loadFragment(new TriageFragment()); });
+        buttonMedicine.setOnClickListener(v -> { loadFragment(new MedicineFragment()); });
+        buttonDaily.setOnClickListener(v -> { loadFragment(new SymptomFragment()); });
+        buttonTechnique.setOnClickListener(v -> { loadFragment(new TechniqueFragment()); });
+        buttonPEF.setOnClickListener(v -> { loadFragment(new PEFFragment()); });
+        buttonProfile.setOnClickListener(v -> { loadFragment(new ProfileFragment()); });
     }
 
     private void loadFragment(Fragment fragment) {
@@ -96,6 +62,28 @@ public class HomeChildFragment extends Fragment {
         transaction.replace(R.id.fragmentContainerView, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    private void fetchBadge() {
+        DatabaseReference myref = FirebaseDatabase.getInstance().getReference("badge").child(uid);
+        myref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String opacity1, opacity2, opacity3;
+                    opacity1 = dataSnapshot.child("perfect-controller/completed").getValue(Boolean.class) ? "1.0" : "0.3";
+                    opacity2 = dataSnapshot.child("high-quality/completed").getValue(Boolean.class) ? "1.0" : "0.3";;
+                    opacity3 = dataSnapshot.child("low-rescue/completed").getValue(Boolean.class) ? "1.0" : "0.3";;
+                    badgeLayout.getChildAt(0).setAlpha(Float.parseFloat(opacity1));
+                    badgeLayout.getChildAt(1).setAlpha(Float.parseFloat(opacity2));
+                    badgeLayout.getChildAt(2).setAlpha(Float.parseFloat(opacity3));
+                }
+            }
+
+            @Override
+            public void onCancelled(com.google.firebase.database.DatabaseError databaseError) {
+            }
+        });
     }
 
 }
