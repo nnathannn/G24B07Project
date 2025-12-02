c5dc0ce435ab5fdcf7f5884afcb7ca5d15eb33b0
Lucia Giovani <lucia@lucias-air.home>
2025-12-01 21:19:12 -0500

    refactor: delete pef class

--- app/src/main/java/com/example/smartair/Zone.java
+++ app/src/main/java/com/example/smartair/Zone.java
@@ -7,31 +7,40 @@ import androidx.annotation.RequiresApi;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 
-public class Zone extends PEF {
+public class Zone extends Item {
+    private double count;
     private String status;
     private double curPB;
 
     public Zone() {}
 
-    public Zone (LocalDateTime date, String id, double count, double curPB) throws IllegalArgumentException {
-        super(date, id, count);
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Zone (String date, String id, double count, double curPB) throws DateTimeParseException, IllegalArgumentException {
+        super(date, id);
+        if (count <= 0) {
+            throw new IllegalArgumentException("PEF should be a positive number");
+        }
+        this.count = count;
         this.curPB = curPB;
         this.status = (count >= 0.8 * curPB ? "Green" : (count >= 0.5 * curPB ? "Yellow" : "Red"));
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public Zone (String date, String id, double count, double curPB) throws DateTimeParseException, IllegalArgumentException {
-        super(date, id, count);
-        this.curPB = curPB;
-        this.status = (count >= 0.8 * curPB ? "Green" : (count >= 0.5 * curPB ? "Yellow" : "Red"));
-    }
+    public double getCount() { return count; }
 
     public String getStatus() { return status; }
 
     public Double getCurPB() { return curPB; }
 
+    // PEF needs to be a positive number
+    public void setCount(double count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("PEF should be a positive number");
+        }
+        this.count = count;
+    }
+    
     public void setStatus(double count, double curPB) throws IllegalArgumentException {
-        super.setCount(count);
+        this.count = count;
         this.curPB = curPB;
         this.status = (count >= 0.8 * curPB ? "Green" : (count >= 0.5 * curPB ? "Yellow" : "Red"));
     }
