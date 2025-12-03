898f30be099ae7b6c31b32ccb977a76224b42d60
Aaron <aaronbsunarto@gmail.com>
2025-12-02 20:34:47 -0500

    feat: finalized tests

--- app/src/test/java/com/example/smartair/SignInPresenterTest.java
+++ app/src/test/java/com/example/smartair/SignInPresenterTest.java
@@ -6,11 +6,14 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import android.content.Intent;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -75,5 +78,48 @@ public class SignInPresenterTest {
         verify(mockModel).signIn(mockPresenter, "fakeuser@gmail.com", "fakepass");
     }
 
+    // startSignIn with parent role
+    @Test
+    public void startSignInTest1() {
+        mockPresenter.startSignIn("parent");
+        ArgumentCaptor<Intent> captor = ArgumentCaptor.forClass(Intent.class);
+        verify(mockView).startActivity(captor.capture());
+        Intent capturedIntent = captor.getValue();
+        assertEquals(HomeParent.class.getName(), capturedIntent.getComponent().getClassName());
+    }
 
+    // startSignIn with provider role
+    @Test
+    public void startSignInTest2() {
+        mockPresenter.startSignIn("provider");
+        ArgumentCaptor<Intent> captor = ArgumentCaptor.forClass(Intent.class);
+        verify(mockView).startActivity(captor.capture());
+        Intent capturedIntent = captor.getValue();
+        assertEquals(HomeProvider.class.getName(), capturedIntent.getComponent().getClassName());
+    }
+
+    // startSignIn with child role
+    @Test
+    public void startSignInTest3() {
+        mockPresenter.startSignIn("parent");
+        ArgumentCaptor<Intent> captor = ArgumentCaptor.forClass(Intent.class);
+        verify(mockView).startActivity(captor.capture());
+        Intent capturedIntent = captor.getValue();
+        assertEquals(ChildActivity.class.getName(), capturedIntent.getComponent().getClassName());
+    }
+
+    // showError with logic error
+    @Test
+    public void showErrorTest1() {
+        mockPresenter.showError("Fake error message", "logic");
+        verify(mockView).displayErrorToast("Logic error: Fake error message");
+    }
+
+    // showError with database error
+    @Test
+    public void showErrorTest2() {
+        mockPresenter.showError("Fake error message", "database");
+        verify(mockView).displayErrorToast("Database error: Fake error message");
+    }
 }
+
